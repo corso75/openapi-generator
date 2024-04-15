2a9fa12084b17e96aaf780238dc4ab16fb22cd2a
corso75 <corso75@mail.ru>
2024-04-15 15:32:05 +0400

    Updated samples/server/petstore/python-flask via bin/generate-samples.sh

--- samples/server/petstore/python-flask/openapi_server/models/pet.py
+++ samples/server/petstore/python-flask/openapi_server/models/pet.py
@@ -3,8 +3,7 @@ from datetime import date, datetime  # noqa: F401
 from typing import List, Dict  # noqa: F401
 
 from openapi_server.models.base_model import Model
-from openapi_server.models.category import Category
-from openapi_server.models.tag import Tag
+
 from openapi_server import util
 
 from openapi_server.models.category import Category  # noqa: E501
